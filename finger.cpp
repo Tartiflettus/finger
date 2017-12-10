04c9b4dd9907695e977775498915e455c6b37284
Creusel <victor.creusel@gmail.com>
2017-12-10 19:27:15 +0100

    Début écriture fingerprint. A faire

---
+++ finger.cpp
@@ -0,0 +1,27 @@
+#include "finger.hpp"
+#include <fstream>
+
+
+//TODO
+int fingerprint(int p, std::string fn)
+{/*
+    std::ifstream file;
+    file.open(fn);
+    if(!file.is_open())
+    {
+        return ECHEC;
+    }
+
+
+    int somme = 0;
+    int puis = 0; //puissance de 256 jusque n, le nombre de blocs de taille 32 bits du fichier
+    char buffer[4]; //32 bits
+    while(file.good()) //lire tout le fichier
+    {
+        file.read(buffer, 4);
+        somme +=
+    }
+
+
+    file.close();
+*/}
---
+++ finger.hpp
@@ -0,0 +1,12 @@
+#ifndef FINGER_HPP_INCLUDED
+#define FINGER_HPP_INCLUDED
+
+const int ECHEC = -1;
+
+/*
+Renvoie le fingerprint de fn, ou ECHEC si échec
+*/
+int fingerprint(int p, std::string fn);
+
+
+#endif // FINGER_HPP_INCLUDED
