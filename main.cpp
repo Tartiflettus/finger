7b5e2757e00637b7160f49e150ee11de14176a43
Creusel <victor.creusel@gmail.com>
2017-12-10 18:51:32 +0100

    pseudoprime et nextprime écrits

--- main.cpp
+++ main.cpp
@@ -6,11 +6,26 @@ using namespace std;
 
 int main()
 {
-    const int x = 9, y = 118, p = 17;
+    const int x = 9, y = 118;
+    int p = 17;
 
 
     cout<< "1: "<< x<< " puissance "<< y<< " mod "<< p<< " = "<< puissance(x, y, p)<< endl;
     cout<< "2: "<< x<< " puissance "<< y<< " mod "<< p<< " = "<< int(pow(x, y))%p<< endl;
+
+
+    cout<< endl;
+    cout<< "1 : "<< p<< (pseudoprime(p) ? " est pseudo-premier" : " n'est pas pseudo-premier")<< endl;
+    p = 2;
+    cout<< "1 : "<< p<< (pseudoprime(p) ? " est pseudo-premier" : " n'est pas pseudo-premier")<< endl;
+
+
+    for(int i=0; i < 5; ++i)
+    {
+        cout<< "nextprime: ";
+        p = nextprime();
+        cout<< p<< " ; pseudo-premier ? "<< pseudoprime(p)<< endl;
+    }
 
     return 0;
 }
--- util.cpp
+++ util.cpp
@@ -1,1 +1,60 @@
+#include "util.hpp"
+#include <random>
+#include <functional> //bind
+#include <chrono> //clock for random seed
 
+
+
+int multiply(int x, int y, int p)
+{
+    int result = 0;
+    while(y)
+    {
+    if(y & 1) result = (result + x) % p;
+    y >>= 1;
+    x = (2*x)%p;
+    }
+    return result;
+}
+
+
+int puissance(int x, int k, int p)
+{
+    if(k == 0)
+    {
+        return 1;
+    }
+    if(k == 1)
+    {
+        return x;
+    }
+    // diviser le problème en 2 sous problèmes égaux
+    const int moitieK = k/2; // une seule opération DIV de x86 ? :D
+    const int reste = k%2;
+    const int moitieRes = puissance(x, moitieK, p);
+
+    //ne pas oublier l'éventuelle mutliplication supplémentaire si k n'est pas divisible par 2
+    return (multiply(moitieRes, moitieRes, p) * (reste == 1 ? x%p : 1))%p;
+}
+
+
+
+bool pseudoprime(int p)
+{
+    return puissance(2, p-1, p) == 1;
+}
+
+
+
+int nextprime()
+{
+    static std::default_random_engine generateur(std::chrono::system_clock::now().time_since_epoch().count());
+    static std::uniform_int_distribution<int> distribution(2, (1<<23) - 1);
+
+    static auto lancer = std::bind ( distribution, generateur );
+
+    int res;
+    while(!pseudoprime(res = lancer())) ;
+
+    return res;
+}
