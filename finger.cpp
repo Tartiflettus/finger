ea1d64cf784f3beac2c2ad43d8dc122e9082580a
Creusel <victor.creusel@gmail.com>
2017-12-16 16:21:21 +0100

    Correction fingerprint

--- finger.cpp
+++ finger.cpp
@@ -18,15 +18,18 @@ int fingerprint(int p, const std::string& fn)
 
     std::vector<char> buffer;
     buffer.reserve(taille);
-    std::copy(std::istream_iterator<char>(file),
-              std::istream_iterator<char>(),
-              std::back_insert_iterator< std::vector<char> >(buffer));
+    while(true)
+    {
+        const char c = file.get();
+        if(file.eof()) break; //quitter la boucle si on a fini de lire le fichier
+        buffer.push_back(c);
+    }
 
     int somme = 0;
     int puismodp = 1;
     for(int i = buffer.size()-1; i >= 0; i--)
     {
-        //std::cout<< buffer[i];
+        //const char c = buffer[i];
         int byte = *((unsigned char*)(void*)&buffer[i]);
         byte = ((byte % p) * puismodp) % p;
         somme = (somme + byte) % p; // somme et buffer sont déjà en modulo p
@@ -52,19 +55,19 @@ int fingerprint(int p, const std::string& fn)
 
     int somme = 0;
     int puismodp = 1; //puissance de 256 jusque n, le nombre d'octets du fichier
-    int buffer;
+    int byte;
     while(file.tellg() >= 0) //lire tout le fichier
     {
         char c = file.get();
-        buffer = *((unsigned char*)(void*)&c);
+        byte = *((unsigned char*)(void*)&c);
         if(file.bad())
         {
             return ECHEC;
         }
         //std::cout<< buffer;
 
-        buffer = ((buffer % p) * puismodp) % p;
-        somme = (somme + buffer) % p; // somme et buffer sont déjà en modulo p
+        byte = ((byte % p) * puismodp) % p;
+        somme = (somme + byte) % p; // somme et buffer sont déjà en modulo p
         puismodp = (puismodp << 8) % p; // * 256^n mod p
         file.seekg(-2, file.cur); //reculer de 2
     }
